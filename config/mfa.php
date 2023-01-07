376f2a704caf15dd248249084373ff54d0d86035
95moja71 <mohammad.abas71@gmail.com>
2023-01-07 15:53:27 +0330

    Add comment to mfa config

--- config/mfa.php
+++ config/mfa.php
@@ -4,34 +4,43 @@ return [
 
 
     'models' => [
-
+        /*
+       * When using the "HasMfa" trait from this package, we need to know which
+       * Eloquent model should be used.
+       */
         'mfa' => MojaHedi\Auth\Models\Mfa::class,
-
-
     ],
 
+
+    /*
+    * When using the "HasMfa" trait from this package, we need to know which
+    * parameter is intended for the message.
+    */
     'message' => 'message',
-    'message_separator' => '{}',
 
     'table_names' => [
 
+        /*
+       * When using the "HasMfa" trait from this package, we need to know which
+       * table should be used to retrieve your mfa.
+       */
         'mfa' => 'mfa',
-
-
     ],
+
 
     'FROM_RAND_NUMBER' => 10000,
     'TO_RAND_NUMBER' => 99999,
+
+    /*
+     * Specify the length of time (in minutes) that the message will be valid for.
+     */
     'ttl' => 5,
+
+    /*
+    * Specify the SMS_API_URL.
+    */
+
     'sms_api_url' => env('SMS_API_URL'),
-
-
-    'sms_params' => [
-        'sender',
-        'message',
-        'method',
-        'receptor',
-    ]
 
 
 ];
